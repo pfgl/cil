290bab788617176c700acb93b4245733d8339eac
Jack Machin <jack.machin125@gmail.com>
2017-06-06 14:21:13 +0100

    ah

--- library/bones.php
+++ library/bones.php
@@ -164,10 +164,11 @@ function bones_scripts_and_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'my_deregister_javascript' );
+
 function my_deregister_javascript() {
     if ( is_page_template ('page-tatton.php') ) {
-        wp_deregister_script( 'guybrush-js' );
-        wp_deregister_style( 'guybrush-stylesheet' );
+        wp_dequeue_script( 'guybrush-js' );
+        wp_dequeue_style( 'guybrush-stylesheet' );
     }
 }
 /*********************
