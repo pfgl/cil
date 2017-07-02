b36a59391df53fbdade2d6fd865e92448b3bda7c
Jack Machin <jack.machin125@gmail.com>
2017-07-02 21:00:13 +0100

    Update footer-tatton.php

--- footer-tatton.php
+++ footer-tatton.php
@@ -36,9 +36,7 @@ CB2 1HH
 			<div class="bg-grey pd-md hidden-xs">
 				<div class='container'>
 					<div class='row'>
-						<div class='col-xs-12 footer__legals'>
-							<p>Tatton Investment Management is a trading style of Cambridge Investments Limited which is authorised and regulated by the Financial Conduct Authority. Cambridge Investments Limited Registered in England and Wales. No. 1370458. Registered Address: Paradigm House, Brooke Court, Wilmslow, Cheshire, SK9 3ND. The value of your investments and the income from them can fluctuate and it is possible that investors may not get back the amount they invested. Past performance is not a guide to future performance. The information contained within the website does not constitute investment advice or a recommendation for any product and you should not make any investment decisions on the basis of it. The information contained within the website is subject to the UK regulatory regime and is therefore primarily targeted at customers in the UK.</p>
-						</div>
+	<php get_sidebar('sidebar-legal-tatton');>
 					</div>
 				</div>
 			</div>
